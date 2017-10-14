4e8a02fe13cc36c06b654a354e3cad8c056a1e7d
Iyad Tamer Agha <iyad@iyads-macbook.intern>
2017-10-14 16:00:35 +0200

    Initial Commit

---
+++ FirebaseAB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseAB
+//
+//  Created by Iyad Tamer Agha on 14.10.17.
+//  Copyright © 2017 Iyad Tamer Agha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
